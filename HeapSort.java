0485865de85493f0de694a70fdc9ad5cc8bd5d2c
Kate_Kim <gxk180021@utdallas.edu>
2021-06-15 01:15:44 -0500

    second commit

--- HeapSort.java
+++ HeapSort.java
@@ -3,22 +3,6 @@ public class HeapSort {
 	static int compare = 0;
 	static int move = 0;
 
-
-	// public static <E extends Comparable<E>> void heapSort(E[] list) {
-	// 	//	Create a Heap of integers
-	// 	Heap<E> heap = new Heap<>();
-		
-	// 	//	Add elements to the heap
-	// 	for (int i = 0; i < list.length; i++)
-	// 		heap.add(list[i]);
-		
-	// 	//	Remove elements from the heap
-	// 	for (int i = list.length -1; i >= 0; i--)
-	// 		list[i] = heap.remove();
-	// }
-
-
-
 	/** Heap sort method */
 	public static <E extends Comparable<E>> void heapSort(E[] list) {
 		//	Create a Heap of integers
@@ -41,7 +25,7 @@ public class HeapSort {
 	
 	
 	public static void main(String[] args) {
-		Integer[] list = {0,1,2,3,4,5,6,7,8,9};
+		Integer[] list = {9,8,7,6,5,4,3,2,1,0};
 		heapSort(list);
 		for(int i = 0; i < list.length; i++)
 			System.out.print(list[i] + " ");
